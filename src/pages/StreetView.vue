ddfa2f6b0d51db8224551e834c597fc5d9bb09c0
Milo Hill <milothill@gmail.com>
2022-02-23 12:59:50 +0000

    missing semi

--- src/pages/StreetView.vue
+++ src/pages/StreetView.vue
@@ -277,8 +277,8 @@ export default {
                 this.playerName = snapshot.child('player').child(this.playerId).val().name || false;
 
                 snapshot.ref.child('player').child(this.playerId).child('isOnline').onDisconnect().set(false).then(() => {
-                    snapshot.ref.child('player').child(this.playerId).child('isOnline').set(true)
-                })
+                    snapshot.ref.child('player').child(this.playerId).child('isOnline').set(true);
+                });
             } else {
                 this.isHost = false;
                 this.playerName = null;
