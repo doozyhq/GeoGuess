089cf54003b289d7ddc01c0cf9b4d5ff37a9c940
Milo Hill <milothill@gmail.com>
2022-02-25 16:13:11 +0000

    Fix debugger

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -251,7 +251,6 @@ export default {
         this.game.mode = this.mode;
         this.game.timeAttack = this.timeAttack;
         this.game.playerName = this.playerName;
-        let size = 0;
 
         if (this.roomName) {
             this.room = firebase.database().ref(`rooms/${this.roomName}`);
@@ -265,8 +264,6 @@ export default {
                     const answeredIds = Object.entries(snapshot.child(`round${round}`).val() || {}).filter(([, v]) => v !== 0).map(([k]) => k);
                     const activeUsers = Object.entries(snapshot.child('player').val() || {}).filter(([, p]) => p.isOnline).map(([k]) => k);
                     const guesses = Object.keys(snapshot.child('guess').val() || {});
-
-                    debugger;
 
                     if (
                         // If Time Attack and 1st true guess finish round
